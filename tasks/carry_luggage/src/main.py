fff9b35c3d9e9eed7fa5120c7bcadd6bed128c24
Serge <syraj.alkhalil7@gmail.com>
2023-11-23 15:40:43 +0000

    chore: add buggy pose estimation

--- tasks/carry_luggage/src/main.py
+++ tasks/carry_luggage/src/main.py
@@ -3,25 +3,33 @@
 import rospy
 import numpy as np
 
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PointStamped, Point
 from sensor_msgs import point_cloud2
-from lasr_vision_msgs.srv import YoloDetection, YoloDetectionRequest
+from std_msgs.msg import String
 from sensor_msgs.msg import Image, PointCloud2
+from lasr_vision_msgs.srv import YoloDetectionRequest, YoloDetection
+from tf_module.srv import TfTransformRequest, TfTransform
+
+from tiago_controllers.controllers import Controllers
 
 class Main:
     def __init__(self):
         self.detect_service = rospy.ServiceProxy('/yolov8/detect', YoloDetection)
-        # wait for service to be available
         self.detect_service.wait_for_service()
+        self.tf_service = rospy.ServiceProxy('tf_transform', TfTransform)
+        self.tf_service.wait_for_service()
+
+        self.baseController = Controllers().base_controller
 
         self.depth = None
+        self.initial_pose = self.baseController.get_current_pose()
         self.cam_subs = rospy.Subscriber("/xtion/rgb/image_raw", Image, self.request_test)
         self.depth_subs = rospy.Subscriber("/xtion/depth_registered/points", PointCloud2, self.do_nothing)
 
     def do_nothing(self, msg):
         self.depth = msg
 
-    def estimate_depth(self, cords_of_person):
+    def estimate_person_coords(self, cords_of_person):
        # Check if depth information is available
         if self.depth is not None:
             try:
@@ -30,16 +38,22 @@ class Main:
                 depth_array = np.array(list(depth_data))
 
                 # Extract depth values for the coordinates of the person
-                person_depth_values = []
+                person_depth_values_x = []
+                person_depth_values_y = []
+                person_depth_values_z = []
                 for cord in cords_of_person:
-                    x, y = int(cord[0]), int(cord[1])
-                    person_depth_values.append(depth_array[y, x, 2])  # Depth is the third element in the array
+                    x, y, z = depth_array[cord]
+                    person_depth_values_x.append(x)
+                    person_depth_values_y.append(y)
+                    person_depth_values_z.append(z)
 
                 # Calculate the average depth
-                average_depth = np.mean(person_depth_values)
+                average_depth_x = np.mean(person_depth_values_x)
+                average_depth_y = np.mean(person_depth_values_y)
+                average_depth_z = np.mean(person_depth_values_z)
 
-                rospy.loginfo("Estimated depth of the person: {}".format(average_depth))
-                return average_depth
+                rospy.loginfo("Estimated depth of the person: {}".format((average_depth_x, average_depth_y, average_depth_z)))
+                return (average_depth_x, average_depth_y, average_depth_z)
 
             except Exception as e:
                 rospy.logerr("Error in estimating depth: {}".format(str(e)))
@@ -48,8 +62,27 @@ class Main:
             rospy.logwarn("Depth information not available.")
             return None
 
-    def estimate_pose(self, average_depth):
-        return Pose()
+    def estimate_pose(self, person_cords):
+        x, y, _ = person_cords
+        centroid = PointStamped()
+
+        centroid.point = Point(x, y, 0)
+        centroid.header = self.depth.header
+        
+        tf_req = TfTransformRequest()
+        tf_req.target_frame = String("map")
+        tf_req.point = centroid
+        
+        response = self.tf_service(tf_req)
+        
+        target_pose = Pose()
+        target_pose.position.x = response.target_point.point.x
+        target_pose.position.y = response.target_point.point.y
+        target_pose.position.z = 0.0
+        target_pose.orientation.w = 1.0
+
+        return target_pose
+
 
     def request_test(self, msg):
         # create request
@@ -65,12 +98,16 @@ class Main:
         for detection in response.detected_objects:
             if detection.name == "person":
                 rospy.loginfo("Found person")
-                
+
                 # cords of person in image
                 cords_of_person = detection.xyseg
-                # depth = self.estimate_depth(cords_of_person)
-                # pose = self.estimate_pose(depth)
-                # self.base_controller.sync_to_pose(pose)
+                person_cords = self.estimate_person_coords(cords_of_person)
+                pose = self.estimate_pose(person_cords)
+
+                rospy.loginfo("initial pose: {}".format(self.initial_pose))
+                rospy.loginfo("pose of person: {}".format(pose))
+                
+                self.baseController.sync_to_pose(pose)
 
 if __name__ == '__main__':
     rospy.init_node('go_to_person')
