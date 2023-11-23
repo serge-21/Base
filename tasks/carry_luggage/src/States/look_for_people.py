b37e9d5fc97a9a02ab1568a7f05d5d4d60c2ad8e
Serge <syraj.alkhalil7@gmail.com>
2023-11-23 21:00:37 +0000

    feat: add head movement

--- tasks/carry_luggage/src/States/look_for_people.py
+++ tasks/carry_luggage/src/States/look_for_people.py
@@ -6,14 +6,30 @@ from sensor_msgs.msg import Image
 
 class LookForPeople(smach.State):
     def __init__(self, default):
-        smach.State.__init__(self, outcomes=['succeeded', 'look_for_people'],
+        smach.State.__init__(self, outcomes=['succeeded'],
                              output_keys=['coords'])
         self.default = default
+        self.motion = [
+            self.default.controllers.head_controller.look_right,
+            self.default.controllers.head_controller.look_straight,
+            self.default.controllers.head_controller.look_left,
+            self.default.controllers.head_controller.look_straight
+        ]
+        self.current_motion = 0
 
     def execute(self, userdata):
-        msg = rospy.wait_for_message('/xtion/rgb/image_raw', Image)
-        
+        # if we don't see a person, look around
+        while True:
+            self.motion[self.current_motion % len(self.motion)]()
+            self.current_motion += 1
+
+            if self.detection(userdata):
+                return "succeeded"
+
+    def detection(self, userdata):
         request = YoloDetectionRequest()
+        msg = rospy.wait_for_message('/xtion/rgb/image_raw', Image)
+
         request.image_raw = msg                # sensor_msgs/Image
         request.dataset = "yolov8n-seg.pt"     # YOLOv8 model, auto-downloads
         request.confidence = 0.7               # minimum confidence to include in results
@@ -21,13 +37,13 @@ class LookForPeople(smach.State):
 
         # send request
         response = self.default.detect_service(request)
-        
+        print(response)
         for detection in response.detected_objects:
             if detection.name == "person":
                 self.default.voice.speak("I see a person")
 
                 # cords of person in image
                 userdata.coords = detection.xyseg
-                return "succeeded"
-            
-        return "look_for_people"
+                return True
+        
+        return False
