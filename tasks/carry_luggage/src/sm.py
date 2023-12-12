eced8c487a3031f0cef9e956704f2a40229b8567
Serge <syraj.alkhalil7@gmail.com>
2023-12-12 20:07:33 +0000

    chore: add failed transition to sm

--- tasks/carry_luggage/src/sm.py
+++ tasks/carry_luggage/src/sm.py
@@ -11,5 +11,5 @@ class CarryLuggage(smach.StateMachine):
         self.robot = Default()
 
         with self:
-            smach.StateMachine.add('START', LookForPeople(self.robot), transitions={'succeeded' : 'GO_TO_PERSON'})
+            smach.StateMachine.add('START', LookForPeople(self.robot), transitions={'succeeded' : 'GO_TO_PERSON', 'failed' : 'failed'})
             smach.StateMachine.add('GO_TO_PERSON', GoToPerson(self.robot), transitions={'succeeded' : 'START',  'failed' : 'failed'})
