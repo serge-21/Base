cc442fb0078383edc22947a3e06447fbc81c40ff
Serge <syraj.alkhalil7@gmail.com>
2024-03-06 16:27:55 +0000

    feat: add detect pointing direction to sm

--- tasks/carry_luggage/src/States/Pointing/detect_pointing_direction.py
+++ tasks/carry_luggage/src/States/Pointing/detect_pointing_direction.py
@@ -1,12 +1,16 @@
 import smach
+from carry_luggage.srv import PointingServiceRequest
 
 class DetectPointingDirection(smach.State):
     def __init__(self, default):
-        smach.State.__init__(self, outcomes=['succeeded', 'failed'])
+        smach.State.__init__(self, outcomes=['Left', 'Right', 'failed'])
         self.robot = default
 
     def execute(self, userdata):
-        resp = self.robot.pointing_detection_service()
-        if resp.direction == "Err":
+        req = PointingServiceRequest()
+        resp = self.robot.pointing_detection_service(req)
+        
+        if resp.direction == "Err" or resp.direction == "Front":
             return "failed"
-        return "succeeded"
+        
+        return resp.direction
--- tasks/carry_luggage/src/sm.py
+++ tasks/carry_luggage/src/sm.py
@@ -5,6 +5,7 @@ from Default import Default
 from States.Follow.look_for_people import LookForPeople
 from States.Follow.go_to_person import GoToPerson
 from States.add_to_costmap import AddToCostMap
+from States.Pointing.detect_pointing_direction import DetectPointingDirection
 
 class CarryLuggage(smach.StateMachine):
     def __init__(self):
@@ -14,4 +15,5 @@ class CarryLuggage(smach.StateMachine):
         with self:
             # smach.StateMachine.add('START', LookForPeople(self.robot), transitions={'succeeded' : 'GO_TO_PERSON', 'failed' : 'failed'})
             # smach.StateMachine.add('GO_TO_PERSON', GoToPerson(self.robot), transitions={'succeeded' : 'START',  'failed' : 'failed'})
-            smach.StateMachine.add('ADD_TO_COSTMAP', AddToCostMap(self.robot), transitions={'succeeded' : 'succeeded', 'failed' : 'failed'})
+            # smach.StateMachine.add('ADD_TO_COSTMAP', AddToCostMap(self.robot), transitions={'succeeded' : 'succeeded', 'failed' : 'failed'})
+            smach.StateMachine.add('DETECT_POINTING_DIRECTION', DetectPointingDirection(self.robot), transitions={'Left' : 'succeeded', 'Right': 'succeeded', 'failed' : 'failed'})
