26bed7db066651414aa04777f15b2457a53a74c3
Serge <syraj.alkhalil7@gmail.com>
2024-04-09 14:09:21 +0100

    chore: add comments and guidance

--- tasks/carry_luggage/src/sm.py
+++ tasks/carry_luggage/src/sm.py
@@ -6,14 +6,22 @@ from States.Follow.look_for_people import LookForPeople
 from States.Follow.go_to_person import GoToPerson
 from States.add_to_costmap import AddToCostMap
 from States.Pointing.detect_pointing_direction import DetectPointingDirection
+from States.Cleanup.return_to_original_pose import ReturnToOriginalPose
 
+'''
+change the commented code to make the robot perform specific tasks. 
+PLEASE ensure the simulation is correct before running the code
+Right now the code waits for the robot to detect a pointing direction and then returns the direction
+but for this to work, the simulation should have a person pointing and the person should be in front of the robot
+'''
 class CarryLuggage(smach.StateMachine):
     def __init__(self):
         smach.StateMachine.__init__(self, outcomes=['succeeded', 'failed'])
         self.robot = Default()
 
         with self:
-            # smach.StateMachine.add('START', LookForPeople(self.robot), transitions={'succeeded' : 'GO_TO_PERSON', 'failed' : 'failed'})
+            # smach.StateMachine.add('START', LookForPeople(self.robot), transitions={'succeeded' : 'succeeded', 'failed' : 'failed'})
             # smach.StateMachine.add('GO_TO_PERSON', GoToPerson(self.robot), transitions={'succeeded' : 'START',  'failed' : 'failed'})
             # smach.StateMachine.add('ADD_TO_COSTMAP', AddToCostMap(self.robot), transitions={'succeeded' : 'succeeded', 'failed' : 'failed'})
+            # smach.StateMachine.add('START', ReturnToOriginalPose(self.robot), transitions={'succeeded' : 'succeeded', 'failed' : 'failed'})
             smach.StateMachine.add('DETECT_POINTING_DIRECTION', DetectPointingDirection(self.robot), transitions={'Left' : 'succeeded', 'Right': 'succeeded', 'failed' : 'failed'})
